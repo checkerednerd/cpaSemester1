9602c666d91d9a3f659cb36a4d8df7613db1bcc9
checkerednerd <ronnyvince@gmail.com>
2018-02-27 11:23:06 -0500

    Random Number Generator
    Generates 5 random numbers between 1 and input.

---
+++ PersonalProjects/Learning/randomNumber/randomNumber/randomNumGen.c
@@ -0,0 +1,28 @@
+/* Ron Vincent
+* 2018-02-27
+* Build a simple RNG program
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(void) {
+	int i, input;
+	time_t t;
+
+	//Initialize RNG
+	srand((unsigned)time(&t));
+
+	//Print 5 numbers between 1 and input;
+	printf("Let's calculate 5 numbers between 1 and ");
+	scanf("%d", &input);
+
+	for (i = 0; i < 5; i++) {
+		printf("%d\n", rand() % input);
+	}
+
+	return 0;
+}
