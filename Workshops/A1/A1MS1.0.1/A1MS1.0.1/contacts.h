31addba5ba7e27636ec7fa611ca60fb28630ef52
checkerednerd <ronnyvince@gmail.com>
2018-03-03 18:01:53 -0500

    Workshop A1MS1.0.1 Added
    In-Lab done, do At-Home

---
+++ Workshops/A1/A1MS1.0.1/A1MS1.0.1/contacts.h
@@ -0,0 +1,36 @@
+/* -------------------------------------------
+Name: Ron Vincent
+Student number: 039567078
+Email: rmvincent@myseneca.ca
+Section: K
+Date: 2018-03-06
+----------------------------------------------
+Assignment: 1
+Milestone:  1
+---------------------------------------------- */
+
+// Structure type Name declaration
+struct Name {
+	char firstName[31];
+	char middleInitial[7];
+	char lastName[36];
+};
+
+// Structure type Address declaration
+// Place your code here...
+struct Address {
+	int streetNumber = 1;
+	char street[40];
+	int apartmentNumber = 1;
+	char postalCode[7];
+	char city[40];
+};
+
+
+// Structure type Numbers declaration
+// Place your code here...
+struct Numbers {
+	char cell[20];
+	char home[20];
+	char business[20];
+};
