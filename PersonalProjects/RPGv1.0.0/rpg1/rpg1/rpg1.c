35300a917c6e587cc022847dfdf32615ce648854
checkerednerd <ronnyvince@gmail.com>
2018-03-06 13:14:04 -0500

    RPG Updated
    Added crit chance, flee chance, addressed armour < hp problems for case 3 (must implement over 1 and 2)

--- PersonalProjects/RPGv1.0.0/rpg1/rpg1/rpg1.c
+++ PersonalProjects/RPGv1.0.0/rpg1/rpg1/rpg1.c
@@ -15,9 +15,10 @@ int main(void) {
 	int usrARM = 1;
 	int skeHP = 20;
 	int skeARM = 2;
-	int dmg = 0;
 
-	int flee = 0;
+	int dmg = 0; //rang for damage roll
+	int crit = 0; //rng for crit chance
+	int flee = 0; //rng for flee chance
 	int choice;
 	time_t t;
 
@@ -46,22 +47,28 @@ int main(void) {
 			dmg = rand() % 3 + 3; //damage is a random value, within a range of 3, starting at (0 + 3), so 3-5
 			printf("You swing your sword at the approaching skeleton...\n");
 			printf("You dealt %d of damage against the skeleton's armor...\n", dmg);
+			crit = rand() % 5;
+			if (crit == 4) {
+				printf("SSHHHHUNK! A critical hit!\n"); //20% chance of a critical for double damage (rolls 0-4)
+				dmg *= 2;
+			}
 			dmg -= skeARM;
-			printf("The skeleton took %d points of damage.\n", dmg);
-			skeHP -= dmg;
+			printf("The skeleton took %d points of damage.\n", abs(dmg));
+			skeHP -= abs(dmg); //HP subtracts to ABSOLUTE value of damage (if armour > damage, the damage would restore HP otherwise)
 			printf("The skeleton has %d HP remaining...\n", skeHP);
 			printf("\n");
 
 			/*insert code block to validate if you defeated skeleton before it hits you back
 			skeleton should only hit you back if you did not defeat it
 			can put skeleton attack code block into seperate function*/
-			if (skeHP > 0) { //if skeleton is not defeated, it will atack
+			if (skeHP > 0) { //if skeleton is not defeated, it will attack
 				dmg = rand() % 4 + 2; //2-5 for skeleton
 				printf("The skeleton swings its rotting hang at you...\n");
 				printf("The skeleton dealt %d of damage against your armor...\n", dmg);
 				dmg -= usrARM;
-				printf("You took %d points of damage.\n", dmg);
-				usrHP -= dmg;
+				printf("You took %d points of damage.\n", abs(dmg));
+				usrHP -= abs(dmg);
+
 				printf("You have %d HP remaining...\n", usrHP);
 				printf("\n");
 
@@ -87,11 +94,18 @@ int main(void) {
 			printf("You bolster your stance defensively...\n");
 			printf("Armor +2 for the rest of the turn.\n");
 
-			if (skeHP > 0) { //if skeleton is not defeated, it will atack
+			if (skeHP > 0) { //if skeleton is not defeated, it will attack
 				dmg = rand() % 4 + 2; //2-5 for skeleton
 				printf("The skeleton swings its rotting hang at you...\n");
 				printf("The skeleton dealt %d of damage against your armor...\n", dmg);
-				dmg -= usrARM;
+
+				if (dmg < usrARM) { //if damage is lower than armour, make damage 0
+					dmg = 0;
+				}
+				else { //else damage will mitigated by armour
+					dmg -= usrARM; 
+				}
+
 				printf("You took %d points of damage.\n", dmg);
 				usrHP -= dmg;
 				printf("You have %d HP remaining...\n", usrHP);
@@ -112,15 +126,21 @@ int main(void) {
 
 		case 4: //Flee battle
 			system("cls");
-			printf("You managed to safely get away...\n");
-			flee = 1;
+			flee = rand() % 2; //50% chance to escape (flee will be either 0 (false) or true (1))
+
+			if (flee == 1) {
+				printf("You managed to safely get away...\n");
+			}
+			else {
+				printf("The Skeleton blocked your path, you could not escape.\n");
+			}
 			break;
 
 		default: //Incorrect choice
 			system("cls");
 		}
 
-	} while (flee == 0 && usrHP > 0 && skeHP > 0);
+	} while (flee == 0 && usrHP > 0 && skeHP > 0); //terminates when user flees or usr/skeHP is 0 or less
 
 	return 0;
 }
