6eb0e1eccbf91a9d04249d737c212663ab527587
checkerednerd <ronnyvince@gmail.com>
2018-03-13 12:05:37 -0400

    Updated MS3 contact.h
    May need to re-enter after submit, not sure if properly implemented

--- Workshops/A1/MS3/contacts.h
+++ Workshops/A1/MS3/contacts.h
@@ -1,28 +1,43 @@
 /* -------------------------------------------
-Name:
-Student number:
-Email:
-Section:
-Date:
+Name: Ron Vincent
+Student number: 039567078
+Email: rmvincent@myseneca.ca
+Section: K
+Date: 2018-03-06
 ----------------------------------------------
 Assignment: 1
-Milestone:  3
+Milestone:  1
 ---------------------------------------------- */
 
 // Structure type Name declaration
 struct Name {
-    char firstName[31];
-    char middleInitial[7];
-    char lastName[36];
+	char firstName[31];
+	char middleInitial[7];
+	char lastName[36];
 };
 
 // Structure type Address declaration
-// Place your code from Milestone #2 here...
+// Place your code here...
+struct Address {
+	int streetNumber;
+	char street[40];
+	int apartmentNumber;
+	char postalCode[8];
+	char city[40];
+};
 
 
 // Structure type Numbers declaration
-// Place your code from Milestone #2 here...
-
-
-// Structure type Contact declaration
 // Place your code here...
+struct Numbers {
+	char cell[20];
+	char home[20];
+	char business[20];
+};
+
+//Structure type Contact declaration
+struct Contact (char Names, char Address, char Numbers) {
+    char name[] = Names;
+    char address[] = Address;
+    char numbers[] = Numbers;
+};
