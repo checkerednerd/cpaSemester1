fa9d78cdc3744c61bf899f46e3cf08eafaea1ddf
checkerednerd <ronnyvince@gmail.com>
2018-03-19 10:24:27 -0400

    A1MS4.1 Added
    Updated to work per program instructions

---
+++ Workshops/A1/A1MS4.1/A1MS4.0.1/contacts.c
@@ -0,0 +1,130 @@
+/* -------------------------------------------
+Name: Ron Vincent
+Student number: 039567078
+Email: rmvincent@myseneca.ca
+Section: K
+Date: 2018-03-18
+----------------------------------------------
+Assignment: 1
+Milestone:  4
+---------------------------------------------- */
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+// This source file needs to "know about" the structures you declared
+// in the header file before referring to those new types:
+// HINT: put the header file name in double quotes so the compiler knows
+//       to look for it in the same directory/folder as this source file
+// #include your contacts header file on the next line:
+
+#include "contacts.h"
+
+char choice = ' ';
+
+void clear(void) { //empties the input buffer when called
+	while (getchar() != '\n')
+		;
+}
+
+// Get and store from standard input the values for Name
+// Put your code here that defines the Contact getName function:
+void getName(struct Name * name) {
+
+	printf("Please enter the contact's first name: ");
+	scanf("%s", name->firstName);
+
+	while (choice != 'n' && choice != 'y' && choice != 'N' && choice != 'Y') {
+		clear(); //clear input buffer
+		printf("Do you want to enter a middle initial(s)? (y or n): ");
+		choice = getchar();
+	}
+
+	if (choice == 'y' || choice == 'Y') {
+		printf("Please enter the contact's middle initial(s): ");
+		scanf("%s", name->middleInitial);
+	}
+	choice = ' '; //clear choice for future use
+
+	printf("Please enter the contact's last name: ");
+	scanf("%s", name->lastName);
+
+	return;
+}
+
+// Get and store from standard input the values for Address
+// Put your code here that defines the Contact getAddress function:
+void getAddress(struct Address * address) {
+
+	printf("Please enter the contact's street number: ");
+	scanf("%d", &address->streetNumber);
+
+	clear(); //clear input buffer for whitespace
+	printf("Please enter the contact's street name: ");
+	scanf("%[^\n]s", address->street); //reads text until new line (overrides default until whitespace)
+
+	while (choice != 'n' && choice != 'y' && choice != 'N' && choice != 'Y') {
+		clear(); //clear input buffer
+		printf("Do you want to enter an apartment number? (y or n): ");
+		choice = getchar();
+	}
+
+	if (choice == 'y' || choice == 'Y') {
+		printf("Please enter the contact's apartment number: ");
+		scanf("%d", &address->apartmentNumber);
+	}
+	choice = ' '; //clear choice for future use
+
+	clear(); //clear input buffer for whitespace
+	printf("Please enter the contact's postal code: ");
+	scanf("%[^\n]s", address->postalCode);
+
+	printf("Please enter the contact's city: ");
+	scanf("%s", address->city);
+
+	return;
+}
+
+// Get and store from standard input the values for Numbers
+// Put your code here that defines the Contact getNumbers function:
+void getNumbers(struct Numbers * numbers) {
+
+	while (choice != 'n' && choice != 'y' && choice != 'N' && choice != 'Y') {
+		clear(); //clear input buffer
+		printf("Do you want to enter a cell phone number? (y or n): ");
+		choice = getchar();
+	}
+
+	if (choice == 'y' || choice == 'Y') {
+		printf("Please enter the contact's cell phone number: ");
+		scanf("%s", numbers->cell);
+	}
+	choice = ' '; //clear choice for future use
+
+	while (choice != 'n' && choice != 'y' && choice != 'N' && choice != 'Y') {
+		clear();
+		printf("Do you want to enter a home phone number? (y or n): ");
+		choice = getchar();
+	}
+
+	if (choice == 'y' || choice == 'Y') {
+		printf("Please enter the contact's home phone number: ");
+		scanf("%s", numbers->home);
+	}
+	choice = ' ';
+
+	while (choice != 'n' && choice != 'y' && choice != 'N' && choice != 'Y') {
+		clear();
+		printf("Do you want to enter a business phone number? (y or n): ");
+		choice = getchar();
+	}
+
+	if (choice == 'y' || choice == 'Y') {
+		printf("Please enter the contact's business phone number: ");
+		scanf("%s", numbers->business);
+	}
+	choice = ' ';
+
+	return;
+}
---
+++ Workshops/A1/A1MS4.1/A1MS4.0.1/contacts.h
@@ -0,0 +1,61 @@
+/* -------------------------------------------
+Name: Ron Vincent
+Student number: 039567078
+Email: rmvincent@myseneca.ca
+Section: K
+Date: 2018-03-18
+----------------------------------------------
+Assignment: 1
+Milestone:  4
+---------------------------------------------- */
+
+// Structure type Name declaration
+struct Name {
+	char firstName[31];
+	char middleInitial[7];
+	char lastName[36];
+};
+
+// Structure type Address declaration
+// Place your code here...
+struct Address {
+	int streetNumber;
+	char street[40];
+	int apartmentNumber;
+	char postalCode[8];
+	char city[40];
+};
+
+
+// Structure type Numbers declaration
+// Place your code here...
+struct Numbers {
+	char cell[20];
+	char home[20];
+	char business[20];
+};
+
+// Structure type Contact declaration
+// Place your code here... (from Milestone 3)
+struct Contact {
+	struct Name name;
+	struct Address address;
+	struct Numbers numbers;
+};
+//------------------------------------------------------
+// Function Prototypes
+//------------------------------------------------------
+
+// ====== Milestone 4 =======
+
+// Get and store from standard input the values for Name
+// Place your code here...
+void getName(struct Name *);
+
+// Get and store from standard input the values for Address
+// Place your code here...
+void getAddress(struct Address *);
+
+// Get and store from standard input the values for Numbers
+// Place your code here...
+void getNumbers(struct Numbers *);
