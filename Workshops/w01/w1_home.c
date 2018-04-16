6da9604abff5ba1a47d1230a01860c2476319048
checkerednerd <ronnyvince@gmail.com>
2018-04-16 13:51:44 -0400

    Keep It Safe

--- Workshops/w01/w1_home.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(void)
-{
-	printf("********************************\n");
-	printf("*** Welcome to C Programming ***\n");
-	printf("********************************\n");
-	return 0;
-}
